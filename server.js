22ca28f9d270751899d94de903975223f25cb499
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-06-19 12:11:26 -0400

    added front end route and GET request

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const port = process.env.PORT || 3001;
-
+const frontRouter = require('./routes/route-frontend');
+const backRouter = require('./routes/route-backend');
 
 const app = express();
-
+//Middleware
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+// listen for requests on port 3001.
 app.listen(port, () => {
     console.log(`API server now on port ${port}!`);
     });
-    
