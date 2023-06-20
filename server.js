9105735bab9a8908bbced3e4a07ef2700ca70a5b
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-06-20 18:13:04 -0400

    changed placement of app use in server.js

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const frontRoute = require('./Develop/routes/route-frontend');
-const backRoute = require('./Develop/routes/route-api');
+const express = require("express");
+const frontRoute = require("./Develop/routes/route-frontend");
+const backRoute = require("./Develop/routes/route-api");
 const port = process.env.PORT || 3000;
-const path = require('path');
-
+const path = require("path");
 
 const app = express();
 //Middleware
-app.use(express.static(path.join(__dirname, '/Develop/public')));
+app.use(express.static(path.join(__dirname, "/Develop/public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// listen for requests on port 3001.
-app.listen(port, () => {
-    console.log(`API server now on port http://localhost:${port}`);
-    });
-
-    // use apiRoutes
+// use apiRoutes
 app.use(frontRoute);
 app.use(backRoute);
+
+// listen for requests on port 3001.
+app.listen(port, () => {
+  console.log(`API server now on port http://localhost:${port}`);
+});
